b96cd168a9b32c8013e7133a555ea50a6fc34fbd
kxtz <kxtz@femboy.vip>
2023-12-25 21:27:18 -0500

    update to latest

---
+++ src/components/channels/BlogViewer.js
@@ -0,0 +1,10 @@
+
+function Blog() {
+    return (
+        <div className="text-white">
+            <p>blog viewer working!!</p>
+        </div>
+    );
+};
+
+export default Blog;
--- src/components/channels/blog.js
+++ src/components/channels/blog.js
@@ -1,7 +1,13 @@
+import React, { lazy } from 'react';
+const BlogViewer = lazy(() => import('./BlogViewer'));
+
 function Blog() {
     return (
-        <div className="text-white">
-            <p>Blog</p>
+        <div className="text-white flex flex-col items-center mx-auto pt-10">
+            <h1 className="text-4xl">kxtz' shitty blog posts</h1>
+            <p className="text-2xl">me rambling about some dumb shit that came to my mind</p>
+            <div className="bg-gray-600 h-2 w-8/12 my-2 mx-auto rounded"></div>
+            <BlogViewer />
         </div>
     );
 };
