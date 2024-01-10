5b58deaf3b2e05602222f274123f697c31da6ba3
kxtzownsu <kxtzownsu@gmail.com>
2024-01-09 23:48:49 -0500

    i just realized that this was on the old domain, wtf?

--- src/components/channels/about.js
+++ src/components/channels/about.js
@@ -1,7 +1,7 @@
 function AboutMe() {
   return (
     <div className="text-white flex flex-col items-center justify-center p-2 h-screen rounded-md overflow-hidden">
-      <img src="https://kxtz.femboy.vip/kxtz.png" alt="me :3" className="rounded-3xl mb-0 scale-75" />
+      <img src="https://kxtz.dev/kxtz.png" alt="me :3" className="rounded-3xl mb-0 scale-75" />
       <h1 className="mb-1 text-2xl font-bold">kxtz</h1>
       <p className="text-center">
         I'm a 14-year-old aspiring software engineer who <br />mainly writes in Python and JS.
