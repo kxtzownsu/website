7cd186126ce9503c2eacad2b52e61d8b34509602
kxtzownsu <kxtzownsu@gmail.com>
2024-04-14 23:01:05 -0400

    idk random commit that isnt finished

---
+++ public/blogs/firstblog.md
@@ -0,0 +1,5 @@
+[//]: # (Title: "KVS")
+[//]: # (Description: "KVS: Kernel Version Switcher")
+[//]: # (Date: "12/25/23")
+# KVS: Kernel Version Switcher
+## 12/25/23
--- src/components/channels/BlogViewer.js
+++ src/components/channels/BlogViewer.js
@@ -1,22 +1,25 @@
-function Blog() {
-    return (
-      <div className=" h-screen w-auto p-10">
-        {/* First Blog Post */}
-        <div className="border border-primary p-4 mb-4 text-center rounded">
-          <h1 className="text-primary">First Blog</h1>
-          <p className="text-primary">Description of the blog...</p>
-          <p className="text-gray-400"><i>12/25/23</i></p>
-        </div>
-  
-        {/* Second Blog Post */}
-        <div className="border border-primary p-4 text-center rounded">
-          <h1 className="text-primary">Second Blog</h1>
-          <p className="text-primary">Description of the blog...</p>
-          <p className="text-gray-400"><i>12/25/23</i></p>
-        </div>
-      </div>
-    );
-  }
-  
-  export default Blog;
-  
+import React from 'react';
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+
+function BlogViewer() {
+  const markdown = `
+  <!-- 
+  Title: "KVS"
+  Description: "KVS: Kernel Version Switcher"
+  Date: "12/25/23"
+  -->
+  # KVS
+  # Kernel Version Switcher
+  <hr>
+  <i>12/25/23</i>
+  `;
+
+  return (
+    <div>
+      <Markdown rehypePlugins={[rehypeRaw]}>{markdown}</Markdown>
+    </div>
+  );
+}
+
+export default BlogViewer;
