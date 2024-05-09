14f5af1c6d169bfc814f1e2654f57a223dd037aa
kxtzownsu <kxtzownsu@gmail.com>
2024-05-09 09:09:32 -0400

    forgor my birthday

--- src/channels/AboutMe.jsx
+++ src/channels/AboutMe.jsx
@@ -4,7 +4,7 @@ export function AboutMe() {
       <img src="/kxtz.png" alt="me :3" className="rounded-full mb-5 w-36 y-36" />
       <h1 className="mb-1 text-2xl font-bold">katelyn <i className="text-sm">(kxtz)</i></h1>
       <p className="text-center">
-        I'm a 14-year-old programmer who <br />mainly writes in Python and JS and uses Arch btw.
+        I'm a 15-year-old programmer who <br />mainly writes in Python and JS and uses Arch btw.
       </p>
     </div>
   );
