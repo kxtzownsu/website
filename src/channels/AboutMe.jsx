ce78168d4b3a28630f2d504d8f3170af8767bc7c
kat <116377025+kxtzownsu@users.noreply.github.com>
2024-05-09 08:51:01 -0400

    feat(AboutMe): pull pfp from github instead of kxtz.dev

--- src/channels/AboutMe.jsx
+++ src/channels/AboutMe.jsx
@@ -1,7 +1,7 @@
 export function AboutMe() {
     return (
       <div className="text-white flex flex-col items-center justify-center p-2 h-screen rounded-md overflow-hidden">
-        <img src="https://kxtz.dev/kxtz.png" alt="me :3" className="rounded-full mb-5 w-36 y-36" />
+        <img src="https://github.com/kxtzownsu.png" alt="me :3" className="rounded-full mb-5 w-36 y-36" />
         <h1 className="mb-1 text-2xl font-bold">kxtz</h1>
         <p className="text-center">
           I'm a 14-year-old aspiring software engineer who <br />mainly writes in Python and JS.
