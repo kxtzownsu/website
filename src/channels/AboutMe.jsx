4ce14f3df0dd7afd2564fbfea668424870470283
kxtzownsu <kxtzownsu@gmail.com>
2024-05-09 09:08:15 -0400

    feat(AboutMe): pull pfp from public/kxtz.png instead of github

--- src/channels/AboutMe.jsx
+++ src/channels/AboutMe.jsx
@@ -1,12 +1,11 @@
 export function AboutMe() {
-    return (
-      <div className="text-white flex flex-col items-center justify-center p-2 h-screen rounded-md overflow-hidden">
-        <img src="https://github.com/kxtzownsu.png" alt="me :3" className="rounded-full mb-5 w-36 y-36" />
-        <h1 className="mb-1 text-2xl font-bold">kxtz</h1>
-        <p className="text-center">
-          I'm a 14-year-old aspiring software engineer who <br />mainly writes in Python and JS.
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="text-white flex flex-col items-center justify-center p-2 h-screen rounded-md overflow-hidden">
+      <img src="/kxtz.png" alt="me :3" className="rounded-full mb-5 w-36 y-36" />
+      <h1 className="mb-1 text-2xl font-bold">katelyn <i className="text-sm">(kxtz)</i></h1>
+      <p className="text-center">
+        I'm a 14-year-old programmer who <br />mainly writes in Python and JS and uses Arch btw.
+      </p>
+    </div>
+  );
+}
